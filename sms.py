8a7e44b7c8868fceb951ccc8997411f21a8cafbe
Ajay singh chouhan <69968374+ajchouhan@users.noreply.github.com>
2021-06-03 14:47:05 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
-account_sid = "AC1cba51c4dd13667ea72cbdba2f196f8d"
-auth_token = "56e07990e4098c076cbe2d6f8a01ce13"
+account_sid = "AC1cba51c4dd136***************************bdba2f196f8d"
+auth_token = "56e07990e4098c0******************************2d6f8a01ce13"
 client = Client(account_sid, auth_token)
 
-message = client.messages.create( body="security alert face detecd", from_='+14243736050', to='+918770542408')
+message = client.messages.create( body="security alert face detecd", from_='+142437***050', to='+91****542408')
 
 print(message.sid)
